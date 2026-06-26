a9300c1ec08abc9738e67517dd94779daf83bbb6
Daffa Galih Pratama <daffagalihpratama2006>
2026-06-26 20:00:30 +0700

    fix migration tugas foreign key

--- database/migrations/2026_05_10_103446_create_tugas_table.php
+++ database/migrations/2026_05_10_103446_create_tugas_table.php
@@ -10,17 +10,31 @@ return new class extends Migration
      * Run the migrations.
      */
     public function up(): void
-    {
-        Schema::create('tugas', function (Blueprint $table) {
-    $table->id();
-    $table->foreignId('kelas_id')->constrained()->onDelete('cascade');
-    $table->foreignId('dosen_id')->constrained('users')->onDelete('cascade');
-    $table->string('judul');
-    $table->text('deskripsi');
-    $table->date('deadline');
-    $table->timestamps();
-});
-    }
+{
+    Schema::create('tugas', function (Blueprint $table) {
+        $table->id();
+
+        $table->unsignedBigInteger('kelas_id');
+        $table->unsignedBigInteger('dosen_id');
+
+        $table->string('judul');
+        $table->text('deskripsi');
+        $table->date('deadline');
+
+        $table->timestamps();
+
+        // foreign key manual (lebih aman di Railway)
+        $table->foreign('kelas_id')
+            ->references('id')
+            ->on('kelas')
+            ->onDelete('cascade');
+
+        $table->foreign('dosen_id')
+            ->references('id')
+            ->on('users')
+            ->onDelete('cascade');
+    });
+}
 
     /**
      * Reverse the migrations.
